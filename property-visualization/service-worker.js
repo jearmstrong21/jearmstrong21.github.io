d7f6d7e3d45716d76bede6b61cfbe49cb7581bbf
Jack Armstrong <mackycheese21@gmail.com>
2020-05-26 15:27:11 -0700

    Update homepage

--- property-visualization/service-worker.js
+++ property-visualization/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8147640acbb5b80b3edb0db4fda281f.js"
+  "/property-visualization/precache-manifest.b2def475dc77626f89623cf9cf196c85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/property-visualization/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
